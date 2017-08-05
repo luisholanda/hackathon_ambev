9dc6946f6a4be92bc909708bd3644835c3bb290d
Luis Holanda <luiscmholanda@gmail.com>
2017-08-05 14:51:46 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+/**
+ * Filename: /home/luiscm/Projects/hackathon_ambev/app.js
+ * Path: /home/luiscm/Projects/hackathon_ambev
+ * Created Date: Saturday, August 5th 2017, 2:47:23 pm
+ * Author: luiscm
+ * 
+ * Copyright (c) 2017 Your Company
+ */
+
+var express = require('express');
+var async = require('async');
+var path = require('path');
+
+// Middlewares
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var compression = require('compression')
+var helmet = require('helmet')
+
+// Routes
+var routes = require('./routes/index')
+
+var app = express();
+
+
+// middlewares setup
+function parallel(middlewares) {
+  return function (req, res, next) {
+    async.each(middlewares, function (mw, cb) {
+      mw(req, res, cb)
+    }, next)
+  }
+}
+
+app.use(parallel([
+  // If nginx is used as load balancing,
+  // you will need to disable some features
+  helmet(),
+  // uncomment after put your favicon in /public
+  // favicon(path.join(__dirname, 'public', 'favicon.ico')),
+  logger('dev'),
+  bodyParser.json(),
+  bodyParser.urlencoded({extended: false}),
+  cookieParser(),
+  express.static(path.join(__dirname, 'public'), {
+    maxAge: 86400000 * 7
+  }),
+  compression({level: 1})
+]))
+
+// Routes setup
+app.use(routes)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
