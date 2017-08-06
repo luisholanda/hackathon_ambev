480992d663b2851e067d7c6d451f3428e48df672
Felipe <felipecoimbra97@gmail.com>
2017-08-06 01:54:54 -0300

    Adding image support and user database feeding

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
-var moongose = require('moongose');
+var mongoose = require('mongoose');
 
-var userSchema = new moongose.Schema({
+var userSchema = new mongoose.Schema({
     name: String,
     self_description: String,
+    photo: {
+              data: Buffer,
+              contentType: String
+            },
 
     rank_name: String,
     level: Number,
