78a53384b971f6630e90ad3e43ca9d498715428e
Luis Holanda <luiscmholanda@gmail.com>
2017-08-06 03:24:19 -0300

    Dot...Dot...Dot...

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('public/build/js/grayscale.js')
+    return gulp.src('public/js/**/*.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
